8f2c19ef39d37567754dc0088d636d79df8c21d1
keerthana <keerthanak1506@gmail.com>
2021-01-30 21:21:40 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {SideNavComponent} from "./side-nav/side-nav.component";
-// import {SearchComponent} from "./search/search.component";
+import {ProjectSetupComponent} from "./project-setup/project-setup.component";
+import {AddUserComponent} from "./add-user/add-user.component";
+import {EditUserComponent} from "./edit-user/edit-user.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, data: { breadcrumb: 'login'}},
-  // {path: 'search', component: SearchStudentComponent, data: {breadcrumb: 'search '}},
-  // {path: 'searches', component: SearchComponent, data: {breadcrumb: 'searches '}},
+  { path: 'edit-user/:postId/edit', component: EditUserComponent, data: { breadcrumb: 'edit' } },
+  { path: 'add', component: AddUserComponent, data: { breadcrumb: 'add' } },
+  // { path: 'project-setup', component: ProjectSetupComponent, data: { breadcrumb: 'project' } },
+  { path: 'student-master', component: ProjectSetupComponent, data: { breadcrumb: 'project' } },
 
   // {path: '', component: SideNavComponent, canActivate: [AuthGuardService], children: [
   {path: '', component: SideNavComponent, children: [
       {path: 'home', component: HomeComponent, data: {breadcrumb: 'home'}},
-    ]
+      ]
   }
   ];
 
