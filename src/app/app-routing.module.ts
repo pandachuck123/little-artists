7b25f441e46bda99342c81b45d52593c46514f99
keerthana <keerthanak1506@gmail.com>
2021-01-30 17:03:53 +0530

    Merge branch 'main' of D:\project\little-artists with conflicts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,35 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {SideNavComponent} from "./side-nav/side-nav.component";
-import {SearchComponent} from "./search/search.component";
-import {GenerateCertificateComponent} from "./generate-certificate/generate-certificate.component";
-import {GalleryComponent} from "./gallery/gallery.component";
-import {MessageComponent} from "./message/message.component";
-import {MultiStudentReportComponent} from "./multi-student-report/multi-student-report.component";
-import {QueryTrackerComponent} from "./query-tracker/query-tracker.component";
-import {UploadStudentComponent} from "./upload-student/upload-student.component";
-import {PasswordCreationComponent} from "./password-creation/password-creation.component";
-import {StudentMasterComponent} from "./student-master/student-master.component";
-import {TeacherReportComponent} from "./teacher-report/teacher-report.component";
-import {TodaySlotComponent} from "./today-slot/today-slot.component";
+// import {SearchComponent} from "./search/search.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'Login', pathMatch: 'full' },
-  { path: 'Login', component: LoginComponent, data: { breadcrumb: 'login'}},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, data: { breadcrumb: 'login'}},
+  // {path: 'search', component: SearchStudentComponent, data: {breadcrumb: 'search '}},
+  // {path: 'searches', component: SearchComponent, data: {breadcrumb: 'searches '}},
+
   // {path: '', component: SideNavComponent, canActivate: [AuthGuardService], children: [
   {path: '', component: SideNavComponent, children: [
-      {path: 'Home', component: HomeComponent, data: {breadcrumb: 'home'}},
-      {path: 'Gallery', component: GalleryComponent, data: {breadcrumb: 'gallery'}},
-      {path: 'Generate-Certificate', component: GenerateCertificateComponent, data: {breadcrumb: 'generate-certificate'}},
-      {path: 'Searches', component: SearchComponent, data: {breadcrumb: 'searches'}},
-      {path: 'Message', component: MessageComponent, data: {breadcrumb: 'Message'}},
-      {path: 'Multi-Student-Report', component: MultiStudentReportComponent, data: {breadcrumb: 'multi-student-report'}},
-      {path: 'Password-Creation', component: PasswordCreationComponent, data: {breadcrumb: 'password-creation'}},
-      {path: 'Query-Tracker', component: QueryTrackerComponent, data: {breadcrumb: 'query-tracker'}},
-      {path: 'Student-Master', component: StudentMasterComponent, data: {breadcrumb: 'student-master'}},
-      {path: 'Teacher-Report', component: TeacherReportComponent, data: {breadcrumb: 'teacher-report'}},
-      {path: 'Today-Slot', component: TodaySlotComponent, data: {breadcrumb: 'today-slot'}},
-      {path: 'Upload-Student', component: UploadStudentComponent, data: {breadcrumb: 'upload-student'}},
+      {path: 'home', component: HomeComponent, data: {breadcrumb: 'home'}},
     ]
   }
   ];
