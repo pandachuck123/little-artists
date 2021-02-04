359e582bc469476b3eef8aa66b206f7936d5b018
SharmilaDeviS <sharmiladevi5795@gmail.com>
2021-02-04 17:31:43 +0530

    commit

--- src/app/student-master/student-master.component.ts
+++ src/app/student-master/student-master.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {DatatableComponent} from "@swimlane/ngx-datatable";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-student-master',
@@ -6,10 +9,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-master.component.scss']
 })
 export class StudentMasterComponent implements OnInit {
+  public searchStudent: FormGroup;
+  public PeriodicElement: any;
+  public pageSize: any;
+  public pageIndex: any;
+  public pageSizeOptions: any;
+  public showFirstLastButtons: any;
+  public length: any;
+  public temp: any;
+  public rows: any;
+  public data: any;
+  public total: any;
+  public selected: any;
 
-  constructor() { }
+  columns = [{ prop: 'name' }, { name: 'Company' }, { name: 'Gender' }];
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+
+  constructor(public fb: FormBuilder, public toast: ToastrService) {
+
+    this.searchStudent = this.fb.group({
+      searchName : [''],
+
+    });
+    this.PeriodicElement = [];
+    this.rows = [];
+    this.temp = [];
+    this.selected = [];
+    this.PeriodicElement = [
+      {id: 133, name: 'Jonathan Ho Jia Jie', teacher: 'Muralindran', class: 'BA', day: 'Monday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 180, name: 'Mansha Sapra', teacher: 'N/A', class: 'BA', day: 'Sunday', timeslot: '02:45 PM to 04:45 PM'},
+      {id: 498, name: 'Apoorva Nair', teacher: 'Muralindran', class: 'JP', day: 'Monday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 555, name: 'Charlotte Teh', teacher: 'Muralindran', class: 'BA', day: 'Monday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 789, name: 'Lee Ching', teacher: 'Muralindran', class: 'BA', day: 'Thursday', timeslot: '02:45 PM to 04:45 PM'},
+      {id: 6847, name: 'Diya Mandal', teacher: 'N/A', class: 'BA', day: 'Monday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 7250, name: 'Laranya Bera', teacher: 'Muralindran', class: 'BA', day: 'Saturday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 7615, name: 'Benjamin Dubs', teacher: 'N/A', class: 'BA', day: 'Thursday', timeslot: '02:45 PM to 04:45 PM'},
+      {id: 7660, name: 'Jason Ling', teacher: 'Muralindran', class: 'BA', day: 'Monday', timeslot: '02:45 PM to 04:45 PM'},
+      {id: 7727, name: 'Shruti Mahindrakar', teacher: 'Muralindran', class: 'BA', day: 'Monday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 133, name: 'Jonathan Ho Jia Jie', teacher: 'N/A', class: 'BA', day: 'Tuesday', timeslot: '01:00 PM to 03:00 PM'},
+      {id: 133, name: 'John', teacher: 'Muralindran', class: 'BA', day: 'Monday', timeslot: '05:00 PM to 07:00 PM'},
+      {id: 133, name: 'Jonathan Ho Jia Jie', teacher: 'N/A', class: 'BA', day: 'Tuesday', timeslot: '05:00 PM to 07:00 PM'}
+
+
+    ];
+
+    this.total = this.PeriodicElement.length;
+    this.rows = this.PeriodicElement;
+    this.temp = this.PeriodicElement;
+
+    this.length = this.PeriodicElement.length;
+
+  }
 
   ngOnInit(): void {
   }
+
+
+  updateFilter(value) {
+    let searchStudentValue= value.searchName;
+    console.log(value,'value....')
+    console.log(searchStudentValue,'searchValue....')
+    const val = searchStudentValue.toLowerCase();
+    const temp = this.temp.filter(function (d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+    this.rows = temp;
+    this.table.offset = 0;
+  }
+
+
 
 }
